ce8f8c64d894a668a305ccb413fff199acc8f09f
Antoine <antoine.houssais.ah@gmail.com>
2024-05-25 23:11:57 +0200

    Add the event for the buttons analyze and clean.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,14 +1,6 @@
 ﻿using System.Diagnostics;
-using System.Text;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PC_Cleaner
 {
@@ -17,9 +9,85 @@ namespace PC_Cleaner
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Members
+        private DirectoryInfo _winTemp;
+        private DirectoryInfo _appTemp;
+        #endregion
+
+        #region Constructor
         public MainWindow()
         {
             InitializeComponent();
+            _winTemp = new DirectoryInfo(@"C:\Windows\Temp");
+            _appTemp = new DirectoryInfo(Path.GetTempPath());
+        }
+        #endregion
+
+        #region Private methods
+        /// <summary>
+        /// Get the size of a folder by adding recursively the size of all the files.
+        /// </summary>
+        /// <param name="dir">Directory to analyze.</param>
+        /// <returns></returns>
+        private long DirSize(DirectoryInfo dir)
+        {
+            try
+            {
+                return dir.GetFiles().Sum(file => file.Length) + dir.GetDirectories().Sum(DirSize);
+            }
+            catch (UnauthorizedAccessException ex) { MessageBox.Show(ex.Message, "Analyze", MessageBoxButton.OK, MessageBoxImage.Error); }
+            return 0;
+        }
+
+        /// <summary>
+        /// Clear recursively the content of a folder.
+        /// </summary>
+        /// <param name="dir">Directory to clear.</param>
+        private void ClearTempData(DirectoryInfo dir)
+        {
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                try { file.Delete(); }
+                catch
+                {
+                    // It may fail depending on the user rights
+                }
+            }
+
+            foreach (DirectoryInfo subDir in dir.GetDirectories())
+            {
+                try { subDir.Delete(recursive: true); }
+                catch
+                {
+                    // It may fail depending on the user rights
+                }
+            }
+        }
+        #endregion
+
+        #region Events
+        private void Button_Analyze_Click(object sender, RoutedEventArgs e)
+        {
+            Space.Content = $"{(DirSize(_winTemp) + DirSize(_appTemp)) / 1_000_000} MB";
+            Date.Content = $"{DateTime.Now:d MMM yyyy HH:mm}";
+            Prompt.Content = "Analyse effectué !";
+        }
+
+        private void Button_Clean_Click(object sender, RoutedEventArgs e)
+        {
+            CleanButton.Content = "NETTOYAGE EN COURS";
+            try
+            {
+                Clipboard.Clear();
+                ClearTempData(_winTemp);
+                ClearTempData(_appTemp);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"An error occured while cleaning your computer: {ex.Message}", "Web site", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            CleanButton.Content = "NETTOYAGE TERMINE";
+            Prompt.Content = "Nettoyage effectué !";
         }
 
         private void Button_History_Click(object sender, RoutedEventArgs e)
@@ -46,5 +114,6 @@ namespace PC_Cleaner
                 MessageBox.Show($"An error occured while opening the web site: {ex.Message}", "Web site", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+        #endregion
     }
 }
